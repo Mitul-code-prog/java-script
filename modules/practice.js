396a57b9da9c7d519f1b77be478e283960b9e8e6
Mitul <amipara2003@gmail.com>
2026-09-22 10:22:36 +0530

    22/09/26

---
+++ modules/practice.js
@@ -0,0 +1,14 @@
+export const sum=0;
+export const addition=(a,b)=>{
+        return a+b;
+}
+export let obj={
+    name:"Ajay",
+    city:"surat"
+}
+export  class Animal {
+        dog(a="German shephard"){
+            console.log(`${a} is barking`);
+            
+        }
+}
